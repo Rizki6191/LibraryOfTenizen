b3e41e00a7b63547f6a142487831aaeb345434cf
Rells <farrelghifari433@gmail.com>
2026-02-19 15:31:39 +0700

    hadeh

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import AuthLayout from "./AuthLayout";
 
 const API_URL = import.meta.env.VITE_API_BASE_URL + "/register";
-
+// aneh
 const RegisterForm = () => {
     const [formData, setFormData] = useState({
         name: "",
